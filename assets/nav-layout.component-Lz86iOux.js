2711f80e2d2df83a6acf19ab2f0f0c83417429d1
hugoheynard <hugoheynard@users.noreply.github.com>
2026-08-11 05:02:01 +0000

    deploy: 42f135e654cc59f2db4de04d616bacb1d60fedc8

---
+++ assets/nav-layout.component-Lz86iOux.js
@@ -0,0 +1,53 @@
+import{u as s,aF as d,s as r,A as c,aG as f,aB as p,ɵ as m,a1 as u,y as a,a2 as i,z as l,aH as _,L as h}from"./index-CrYE53GN.js";import{F as v}from"./nav-layout.context-B3-JHDup.js";const g=[[["","tabNav",""]],"*"],y=["[tabNav]","*"],x=32;class e{placement=s("top");foldAt=s(720,{transform:d});folded=r(!1);stacked=c(()=>this.placement()==="top"||this.folded());width=f();constructor(){p(()=>this.measure(this.width()))}measure(t){if(t===0)return;const n=this.foldAt();!this.folded()&&t<=n?this.folded.set(!0):this.folded()&&t>n+x&&this.folded.set(!1)}static ɵfac=function(n){return new(n||e)};static ɵcmp=m({type:e,selectors:[["fold-nav-layout"]],hostVars:2,hostBindings:function(n,o){n&2&&h("is-row",!o.stacked())},inputs:{placement:[1,"placement"],foldAt:[1,"foldAt"]},exportAs:["foldNavLayout"],features:[_([{provide:v,useExisting:e}])],ngContentSelectors:y,decls:4,vars:0,consts:[[1,"tl-nav"],[1,"tl-body"]],template:function(n,o){n&1&&(u(g),a(0,"div",0),i(1),l(),a(2,"div",1),i(3,1),l())},styles:[`@charset "UTF-8";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[_nghost-%COMP%] {
+  display: flex;
+  flex-direction: column;
+  
+
+  gap: var(--fold-nav-layout-gap, var(--fold-space-lg, 16px));
+  min-width: 0;
+}
+
+
+
+
+
+.is-row[_nghost-%COMP%] {
+  flex-direction: row;
+}
+
+
+
+
+
+.tl-nav[_ngcontent-%COMP%] {
+  display: grid;
+  min-width: 0;
+}
+
+
+.is-row[_nghost-%COMP%]   .tl-nav[_ngcontent-%COMP%] {
+  flex: 0 0 var(--fold-nav-layout-rail-width, var(--fold-rail-tertiary, 200px));
+}
+
+.tl-body[_ngcontent-%COMP%] {
+  flex: 1 1 auto;
+  min-width: 0;
+}`]})}export{e as FoldNavLayoutComponent};
--- assets/nav-layout.context-e72G8h2k.js
+++
@@ -1,1 +0,0 @@
-import{ak as o}from"./index-F5cuq_px.js";const e=new o("FOLD_NAV_LAYOUT");export{e as F};
