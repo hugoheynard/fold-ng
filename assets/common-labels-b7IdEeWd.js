33adac958ffb78e9af6f0a1e8267711faaf8226f
hugoheynard <hugoheynard@users.noreply.github.com>
2026-08-26 04:58:56 +0000

    deploy: e6d6c2daed016cc9b02833f5861504efd4732181

---
+++ assets/common-labels-b7IdEeWd.js
@@ -0,0 +1,1 @@
+import{aj as o}from"./index-gi_qXDin.js";const n={optional:"optional",info:"More information",clear:"Clear",loading:"Loading..."},L=new o("FOLD_COMMON_LABELS",{factory:()=>n});export{L as F};
---
+++ assets/nav-layout.context-CCi5H72K.js
@@ -0,0 +1,1 @@
+import{aj as o}from"./index-gi_qXDin.js";const e=new o("FOLD_NAV_LAYOUT");export{e as F};
